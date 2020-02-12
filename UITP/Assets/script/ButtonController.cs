53c75ce2e1b73016aa51467dc035df109746b6a8
Gabriel <takedadesmullier@gmail.com>
2020-02-12 10:00:00 +0100

    AddProject
    Adding Project

---
+++ UITP/Assets/script/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonController : MonoBehaviour
+{
+
+    [SerializeField] private List<Text> script;
+    [SerializeField] private TextMeshProUGUI textMeshProGUI = default;
+    private int index = 0;
+    // Start is called before the first frame update
+
+    public void Change()
+    {
+        textMeshProGUI.text = script[index].TextString;
+
+        if (index < script.Count)
+        {
+            index++;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ UITP/Assets/script/Text.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Text", menuName = "Text")]
+public class Text : ScriptableObject
+{
+
+    [SerializeField] private string text;
+    public string TextString => text;
+}
